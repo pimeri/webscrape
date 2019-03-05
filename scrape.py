0fa8c21bf929746c29258a0ec33ef4d3a5f162db
pimeri <pablo.imeri@gmail.com>
2019-03-05 13:12:07 -0800

    testing stuff

--- scrape.py
+++ scrape.py
@@ -1,41 +1,60 @@
 import urllib2
-from bs4 import BeautifulSoup
-from time import sleep
 import re
+import sys
+import time
+import datetime
+# import re
 
-ticker = 'MSFT'
+start_time = time.time()
 
+
+date = sys.argv[-1]
+tickers = sys.argv[1:-1]
+failed_tickers = []
 urlTemplate = "https://finance.yahoo.com/quote/TICKER/PAGE?p=TICKER"
-
-url = urlTemplate.replace("TICKER",ticker).replace("PAGE","profile")
-
-
-page = urllib2.urlopen(url).read()
+failed_tickers = []
 
 
-historyUrl = "https://finance.yahoo.com/quote/TICKER/history?period1=1551081600&period2=1551254400&interval=1d&filter=history&frequency=1d"
+try:
+	dt = datetime.datetime.strptime(date, '%m-%d-%Y')
+	endTime = int((time.mktime(dt.timetuple()) - 3600))
+	startTime = endTime - 86400
+	
+except:
+	print 'Error: invalid date format, must be mm-dd-yyyy'
+	exit()
 
-# print page.read()
-# data-reactid="80">([0-9]*.[0-9]*B)
 
-# profile page
-m1 = re.search( 'profile for (.*?) \(', page)
-m  = re.search('founded in (.*) and .*quartered in ([a-zA-z]*, [a-zA-z]*)',page)
+# exit()
 
-# 
-url = urlTemplate.replace("TICKER",ticker).replace("/PAGE","")
-print url
-# m2 = re.search( 'data-reactid="80">([0-9]*.[0-9]*B)', page)
 
-# print m.group(1)
+for ticker in tickers:
+	try:
+		url_start = time.time()
+		url = urlTemplate.replace("TICKER",ticker).replace("PAGE","profile")
+		# print ("--- %s seconds for the request --- " % (time.time() - url_start))
+		temp_page = urllib2.urlopen(url)
+		print ("--- %s seconds for the request --- " % (time.time() - url_start))
+		read_start = time.time()
+		page = temp_page.read()
+		print ("--- %s seconds for the request --- " % (time.time() - read_start))
+		# page = urllib2.urlopen(url).read()
+		# profile page
+		m1 = re.search( 'profile for (.*?) \(', page)
+		m  = re.search('founded in (.*) and .* in ([a-zA-z\s]*, [a-zA-z]*)',page)
+		m2 = re.search('\"fullTimeEmployees\":([0-9]*)', page)
 
-# print url
+		if (m1 == None or m == None or m2 == None):
+			raise Exception('failed ticker')
+		print 'name\t\t: ', m1.group(1)
+		print 'founded\t\t: ' , m.group(1)
+		print 'employees\t:' ,  m2.group(1)
+		print 'Headquarters\t: ', m.group(2)
+		print 'Prev close:\t:', " void"
+		print 'open price:\t:', " void \n"
+	except:
+		failed_tickers.append(ticker)
+	# historyUrl = "https://finance.yahoo.com/quote/TICKER/history?period1=1551081600&period2=1551254400&interval=1d&filter=history&frequency=1d"
 
-print 'name\t\t: ', m1.group(1)
-print 'founded\t\t: ' , m.group(1)
-print 'employees\t:' ,  " void"
-print 'Headquarters\t: ', m.group(2)
-print 'Prev close:\t:', " void"
-print 'open price:\t:', " void"
-# print 'Market cap:\t:', m2
-
+print 'failed tickers: ', failed_tickers
+print("--- %s seconds ---" % (time.time() - start_time))
