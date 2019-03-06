50efcfee98d48634e6b147e13f7038a06e48fc38
pimeri <pablo.imeri@gmail.com>
2019-03-06 01:01:27 -0800

    complete

--- scrape.py
+++ scrape.py
@@ -3,14 +3,20 @@ import re
 import sys
 import time
 import datetime
+import tableController
+
 
 start_time 		= time.time()
 date 			= sys.argv[-1]
 tickers 		= sys.argv[1:-1]
 failed_tickers 	= []
+# this session object allows me to reuse one TCP connection which makes a significant difference in speed
 session			= requests.Session()
-urlTemplate 	= "https://finance.yahoo.com/quote/TICKER?p=TICKER"
-urlTemplate2 	= "https://finance.yahoo.com/quote/TICKER/history?period1=TIME1&period2=TIME2&interval=1d&filter=history&frequency=1d"
+urlTemplate 	= "https://finance.yahoo.com/quote/{TICKER}?p={TICKER}"
+urlTemplate2 	= "https://finance.yahoo.com/quote/{TICKER}/history?period1={PERIOD1}&period2={PERIOD2}&interval=1d&filter=history&frequency=1d"
+
+# here I attempt to convert the user input date to unix time
+#  note I leave it in PST(the dafult) as it works better with hte NYSE stock exchange open time
 
 try:
 	dt = datetime.datetime.strptime(date, '%m-%d-%Y')
@@ -20,36 +26,44 @@ except:
 	print 'Error: invalid date format, must be mm-dd-yyyy'
 	exit()
 
+tableController.initTable()
 for ticker in tickers:
 	try:
-		url = urlTemplate.replace("TICKER",ticker).replace("PAGE","profile")
+		url = urlTemplate.format(TICKER=ticker)
 		page = session.get(url).text
 
+		# regular expressions to capture the data I need
 		m1 = re.search( 'Summary for (.*?) -', page)
-		m2 = re.search('founded in (.*?) and .*? in ([a-zA-z\s]*, [a-zA-z]*)',page)
+		m2 = re.search('founded in (.*?) and .*? in ([a-zA-z\s]*, [a-zA-z]*)', page)
 		m3 = re.search('\"fullTimeEmployees\":([0-9]*)', page)
 		m5 = re.search( ticker + '":{.*"marketCap":{"raw":([0-9]*)', page)
 
-		url = urlTemplate2.replace("TICKER",ticker).replace("TIME1",str(startTime)).replace("TIME2",str(endTime))
+		url = urlTemplate2.format(TICKER=ticker,PERIOD1=startTime,PERIOD2=endTime)
 		page = session.get(url).text
 		m4 = re.search('"prices":.*"open":([0-9.]*).*"close":([0-9.]*)',page)
 
-		if (m1 == None or m2 == None or m3 == None or m4 == None ):
+		if (m1 == None or m2 == None or m3 == None or m4 == None or m5 == None):
 			raise Exception('failed ticker')
-		
-		print 'name\t\t: '		, m1.group(1)
-		print 'founded\t\t: ' 	, m2.group(1)
-		print 'employees\t: ' 	, m3.group(1)
-		print 'City\t\t: '		, m2.group(2).split(", ")[0]
-		print 'State\t\t: '		, m2.group(2).split(", ")[1]
-		print 'Prev close\t: '	, m4.group(1)
-		print 'open price\t: '	, m4.group(2)
-		print 'marketCap\t: ' 	, m5.group(1), "\n"
+		# here I pack all the capture groups from the regexes into a tuple and send it to the
+		# sql controller 
+		statement = (
+				m1.group(1),
+				m2.group(1),
+				m3.group(1),
+				m2.group(2).split(", ")[0],
+				m2.group(2).split(", ")[1],
+				m4.group(1),
+				m4.group(2),
+				m5.group(1),
+				date
+			)
+		tableController.insert(statement)
 	except:
 		failed_tickers.append(ticker)
 		
 if failed_tickers:
 	print 'failed tickers: ', failed_tickers
+
+tableController.printTable()
 print("--- Finished in %s seconds ---" % (time.time() - start_time))
 
-	
---
+++ tableController.py
@@ -0,0 +1,61 @@
+import sqlite3
+import pandas
+
+pandas.set_option('display.max_colwidth',1000)
+
+def initTable():
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	cursor.execute('''CREATE TABLE IF NOT EXISTS stocks 
+             (id INTEGER primary key, 
+             company text, 
+             founded int,
+             employees int,
+             city text,
+             state text,
+             prevclose real,
+             openprice real,
+             marketcap real,
+             date      text)
+
+          ''')
+	conn.commit()
+	conn.close()
+
+def insert(data):
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO stocks VALUES (null,?,?,?,?,?,?,?,?,?)", data)
+	conn.commit()
+	conn.close()
+
+def printTable():
+	conn = sqlite3.connect('stocks.db')
+	print pandas.read_sql_query("SELECT * FROM stocks", conn)
+	conn.close()
+
+
+def printByCompany(companyName):
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	t = (companyName,)
+	print pandas.read_sql_query("SELECT * FROM 'stocks' WHERE company = ? ",conn,params=t)
+	
+
+def printByDate(date):
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	t = (date,)
+	print pandas.read_sql_query("SELECT * FROM 'stocks' WHERE date = ? ",conn,params=t)
+
+def printByState(state):
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	t = (state,)
+	print pandas.read_sql_query("SELECT * FROM 'stocks' WHERE state = ? ",conn,params=t)
+
+def printByOpen(price):
+	conn = sqlite3.connect('stocks.db')
+	cursor = conn.cursor()
+	t = (price,)
+	print pandas.read_sql_query("SELECT * FROM 'stocks' WHERE openprice >= ? ",conn,params=t)
